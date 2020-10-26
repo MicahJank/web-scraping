8a5cd45a3788443b2baf44355f3483439c2d8504
MicahJank <MicahJank@users.noreply.github.com>
2020-10-25 22:14:31 -0500

    still working

--- scrape.js
+++ scrape.js
@@ -1,4 +1,4 @@
 const request = require('request');
-const cheerio = require('cheerio');
+const cheerio = require('cheerio'); 
 
 request('https://www.amazon.com/b/ref=bhp_brws_awrd?ie=UTF8&node=6960520011&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-leftnav&pf_rd_r=CTRSGPNFHADGHX8YHJC0&pf_rd_r=CTRSGPNFHADGHX8YHJC0&pf_rd_t=101&pf_rd_p=dfe90ba6-2174-4c8c-9c55-9e9b4d012467&pf_rd_p=dfe90ba6-2174-4c8c-9c55-9e9b4d012467&pf_rd_i=283155')
